fadaae67b8ac268e3abb00df71e782ecc48d0911
Suhrid Singh <suhrids88@gmail.com>
2026-02-02 00:14:34 +0530

    Create majority_element.cpp

---
+++ majority_element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+       int n = nums.size();
+
+       sort(nums.begin(),nums.end());
+
+       if(n%2==0){
+        return nums[(n/2)-1];
+       }
+       else{
+        return nums[n/2];
+       }
+
+
+        
+    }
+};
